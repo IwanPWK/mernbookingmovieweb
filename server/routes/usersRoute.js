fc7dc3168856e04ad75205315b58f45380ef6a68
IwanPWK <koadhyow@gmail.com>
2023-12-13 15:18:29 +0700

    Part 10

--- server/routes/usersRoute.js
+++ server/routes/usersRoute.js
@@ -1,8 +1,7 @@
 const router = require("express").Router();
 const User = require("../models/userModel");
 const bcrypt = require("bcryptjs");
-const jwt = require("jsonwebtoken");
-const authMiddleware = require("../middlewares/authMiddleware");
+const jwt = require("jsonwebtoken"); //Part 10
 
 // register a new user
 router.post("/register", async (req, res) => {
@@ -28,6 +27,49 @@ router.post("/register", async (req, res) => {
     res.send({
       success: true,
       message: "Registration Successfull , Please login",
+    });
+  } catch (error) {
+    res.send({
+      success: false,
+      message: error.message,
+    });
+  }
+});
+
+// login a user
+router.post("/login", async (req, res) => {
+  try {
+    // check if user exists
+    const user = await User.findOne({ email: req.body.email });
+    if (!user) {
+      return res.send({
+        success: false,
+        message: "User does not exist",
+      });
+    }
+
+    // check if password is correct
+    const validPassword = await bcrypt.compare(
+      req.body.password,
+      user.password
+    );
+
+    if (!validPassword) {
+      return res.send({
+        success: false,
+        message: "Invalid password",
+      });
+    }
+
+    // create and assign a token
+    const token = jwt.sign({ userId: user._id }, process.env.jwt_secret, {
+      expiresIn: "1d",
+    });
+
+    res.send({
+      success: true,
+      message: "User logged in successfully",
+      data: token,
     });
   } catch (error) {
     res.send({
