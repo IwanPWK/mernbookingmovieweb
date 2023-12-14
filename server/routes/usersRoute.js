382c1dc199d2c0154af7f5a37a801600fc85a271
IwanPWK <koadhyow@gmail.com>
2023-12-14 12:39:11 +0700

    Part 12

--- server/routes/usersRoute.js
+++ server/routes/usersRoute.js
@@ -79,4 +79,21 @@ router.post("/login", async (req, res) => {
   }
 });
 
+// get user details by id
+router.get("/get-current-user", authMiddleware, async (req, res) => {
+  try {
+    const user = await User.findById(req.body.userId).select("-password");
+    res.send({
+      success: true,
+      message: "User details fetched successfully",
+      data: user,
+    });
+  } catch (error) {
+    res.send({
+      success: false,
+      message: error.message,
+    });
+  }
+});
+
 module.exports = router;
