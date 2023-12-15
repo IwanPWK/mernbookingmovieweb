ab45335ca18e39f63990ea949cf7196182042959
IwanPWK <koadhyow@gmail.com>
2023-12-15 15:58:12 +0700

    Part 16

--- client/src/components/ProtectedRoute.js
+++ client/src/components/ProtectedRoute.js
@@ -70,9 +70,42 @@ function ProtectedRoute({ children }) {
 
   return (
     user && (
-      <div>
-        {user.name}
-        {children}
+      <div className="layout p-1">
+        <div className="header bg-primary flex justify-between p-2">
+          <div>
+            <h1
+              className="text-2xl text-white cursor-pointer"
+              onClick={() => navigate("/")}
+            >
+              SHEYMOVIES
+            </h1>
+          </div>
+
+          <div className="bg-white p-1 flex gap-1">
+            <i className="ri-shield-user-line text-primary"></i>
+            <h1
+              className="text-sm underline"
+              onClick={() => {
+                if (user.isAdmin) {
+                  navigate("/admin");
+                } else {
+                  navigate("/profile");
+                }
+              }}
+            >
+              {user.name}
+            </h1>
+
+            <i
+              className="ri-logout-box-r-line ml-2"
+              onClick={() => {
+                localStorage.removeItem("token");
+                navigate("/login");
+              }}
+            ></i>
+          </div>
+        </div>
+        <div className="content mt-1 p-1">{children}</div>
       </div>
     )
   );
